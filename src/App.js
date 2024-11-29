b9a5ad7e0375f0a75c8042b4a9c4875e40bc0ba0
Charan Romi <charanromi325@gmail.com>
2024-11-29 07:27:29 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-//import logo from './logo.svg';
 import './App.css';
 import AgeCalculator from './components/AgeCalculator';
 import BackgroundAnimation from './components/Animation';
@@ -6,13 +5,13 @@ import Calculator from './components/Calculator';
 
 function App() {
   return (
-    <div className="flex justify-center items-start space-x-4 p-5">
+    <div className="flex flex-col justify-center items-center min-h-screen space-y-16 p-10">
       <BackgroundAnimation />
-      <div className="w-2/5 mt-20">
-          <Calculator />
+      <div className="w-full max-w-md mt-5">
+        <Calculator />
       </div>
-      <div className="w-2/5 mt-20">
-          <AgeCalculator />
+      <div className="w-full max-w-md mt-20">
+        <AgeCalculator />
       </div>
     </div>
   );
